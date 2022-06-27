50b39bd3d72b9f14af30aeda9a05964e8fe0d5fa
Tyler Latimer <trlatimer95@gmail.com>
2022-06-26 22:23:18 -0700

    Added socket zone that follows player

---
+++ Assets/FollowToTheSide.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowToTheSide : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + Vector3.up * offset.y
+            + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
+            + Vector3.ProjectOnPlane(target.forward, Vector3.forward).normalized * offset.z;
+
+        transform.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+    }
+}
